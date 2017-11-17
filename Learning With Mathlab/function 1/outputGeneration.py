6753f961a452eb1c6d9be978ad82cbf5fcbbcc90
Nazanin Sabri <nazanin.sabrii@gmail.com>
2017-11-17 09:29:13 +0330

    attempting to solve the project

---
+++ Learning With Mathlab/function 1/outputGeneration.py
@@ -0,0 +1,39 @@
+import random 
+import math
+def calcFunc(a, b, c):
+	if a==0:
+		print("Nan")
+	else:
+		sorat = (math.pow(a, b)+math.sqrt(math.fabs(c*c-b*b)))
+		makhraj = (math.cos(c)*math.log2(math.fabs(a)))
+		if makhraj==0:
+			print("Nan")
+		else:
+			print(sorat/makhraj)
+			fileA = open("testAA.txt","a")
+			fileA.write(str(a))
+			fileA.write("	")
+			fileA.close()
+			fileB = open("testBB.txt","a")
+			fileB.write(str(b))
+			fileB.write("	")
+			fileB.close()
+			fileC = open("testCC.txt","a")
+			fileC.write(str(c))
+			fileC.write("	")
+			fileC.close()
+			fileRes = open("testResRes.txt","a")
+			fileRes.write(str(sorat/makhraj))
+			fileRes.write("	")
+			fileRes.close()
+
+if __name__ == '__main__':
+	for i in range(300):
+		a = random.randint(-20, 20)
+		print(a)
+		b = random.randint(-10, 10)
+		print(b)
+		c = random.randint(1, 50)
+		print(c)
+		print("The result:")
+		calcFunc(a, b, c)
--- Learning With Mathlab/function 2/newFunc.py
+++ Learning With Mathlab/function 2/newFunc.py
@@ -4,31 +4,31 @@ def calcFunc(a, b, c):
 	if a==0:
 		print("Nan")
 	else:
-		sorat = (math.pow(a, b)+math.sqrt(math.fabs(c*c-b*b)))
-		makhraj = (math.cos(c)*math.log2(math.fabs(a)))
+		makhraj = (math.pow(a, b)+math.sqrt(math.fabs(c*c-b*b)))
+		sorat = (math.cos(c)*math.log2(math.fabs(a)))
 		if makhraj==0:
 			print("Nan")
 		else:
-			print(sorat/makhraj)
-			fileA = open("testA.txt","a")
+			print(math.fabs(sorat/makhraj))
+			fileA = open("A.txt","a")
 			fileA.write(str(a))
-			fileA.write("\n")
+			fileA.write("	")
 			fileA.close()
-			fileB = open("testB.txt","a")
+			fileB = open("B.txt","a")
 			fileB.write(str(b))
-			fileB.write("\n")
+			fileB.write("	")
 			fileB.close()
-			fileC = open("testC.txt","a")
+			fileC = open("C.txt","a")
 			fileC.write(str(c))
-			fileC.write("\n")
+			fileC.write("	")
 			fileC.close()
-			fileRes = open("testRes.txt","a")
-			fileRes.write(str(sorat/makhraj))
-			fileRes.write("\n")
+			fileRes = open("Res.txt","a")
+			fileRes.write(str(math.fabs(sorat/makhraj)))
+			fileRes.write("	")
 			fileRes.close()
 
 if __name__ == '__main__':
-	for i in range(300):
+	for i in range(100):
 		a = random.randint(-20, 20)
 		print(a)
 		b = random.randint(-10, 10)
